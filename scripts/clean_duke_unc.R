8ef350d682737037a558231232e92142c002a8b2
Jesse Raab <jesse.r.raab@gmail.com>
2023-04-15 10:30:01 -0400

    saved script to fix the basketball data

--- scripts/clean_duke_unc.R
+++ scripts/clean_duke_unc.R
@@ -8,8 +8,8 @@ cnames <- c('row', 'season', 'conf', 'overall_w', 'overall_l', 'overall_w_l_perc
             'conf_w', 'conf_l', 'conf_w_l_percent', 'srs', 'sos', 'pts_for_avg', 'pts_against_avg', 
             'ap_pre', 'ap_high', 'ap_final', 'tourney', 'coach') 
 
-duke <- read_csv(duke_file, skip = 2, col_names = cnames) |> mutate('BadGuys')
-unc <- read_csv(unc_file, skip = 2, col_names = cnames) |> mutate('GoodGuys')
+duke <- read_csv(duke_file, skip = 2, col_names = cnames) |> mutate(team = 'BadGuys')
+unc <- read_csv(unc_file, skip = 2, col_names = cnames) |> mutate(team = 'GoodGuys')
 
 combined <- bind_rows(duke, unc)
 write_csv(combined, file = 'GitHub/GNET749_RNAseq/data/duke_unc_hoops.csv')
