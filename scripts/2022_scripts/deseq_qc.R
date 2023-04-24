2ae355898cf8c6a772ddd46a0f513dfdc6d4558b
Jesse Raab <jesse.r.raab@gmail.com>
2023-04-24 11:48:31 -0400

    pre-class 5 update

--- scripts/2022_scripts/deseq_qc.R
+++ scripts/2022_scripts/deseq_qc.R
@@ -6,38 +6,56 @@ library(ComplexHeatmap)
 
 # Lets load our outuput from our DESeq results
 load('data/DE_output.Rda')
-# This will load two variables into our environment , des and res
+# This will load two variables into our environment, des and res
 des <- estimateSizeFactors(des)
-
-
 # get the raw count data
 raw <- counts(des) 
 normalized <- counts(des, normalize = T)
-
-sample_info <- colData(des) %>% as.data.frame() # gets the sample information from the summarized experiment
+sample_info <- colData(des) |> as.data.frame() # gets the sample information from the summarized experiment
 sample_info
 
 ################################################################################# 
 # pivot data into long form
-raw_l <- raw %>% 
-   as.data.frame() %>% 
-   rownames_to_column() %>% 
+raw_l <- raw |> 
+   as.data.frame() |> 
+   rownames_to_column() |> 
    pivot_longer(names_to = 'Sample', values_to = 'count', -rowname)  
 raw_l
-norm_l <- normalized %>% 
-   as.data.frame() %>% 
-   rownames_to_column() %>% 
+norm_l <- normalized |> 
+   as.data.frame() |> 
+   rownames_to_column() |> 
    pivot_longer(names_to = 'Sample', values_to = 'count', -rowname)  
 norm_l
-raw_l <- raw_l %>%  left_join(sample_info, by = 'Sample' ) %>% mutate(method = 'raw')
-norm_l <- norm_l %>% left_join(sample_info, by = 'Sample') %>% mutate(method = 'norm')
+raw_l <- raw_l |>  left_join(sample_info, by = 'Sample' ) |> mutate(method = 'raw')
+norm_l <- norm_l |> left_join(sample_info, by = 'Sample') |> mutate(method = 'norm')
 all_counts <- bind_rows(raw_l, norm_l)
 all_counts
 #################################################################################  
-# Now you can plot them together to see how the average counts per sample differ
-all_counts %>% 
-   group_by(Sample, Group, method) %>% 
-   summarise(total_reads = mean(count, na.rm = T) )  %>% 
+# Now you can plot them together to see how the  counts per sample differ
+all_counts |> 
+   ggplot(aes(x = Sample, y = log2(count+1), fill = Group)) + 
+   geom_boxplot() + 
+   coord_flip() + 
+   facet_wrap(~method)
+
+# why use log2 counts
+all_counts |> 
+  ggplot(aes(x = count, fill = method)) + 
+  geom_density()
+ 
+all_counts |> 
+  ggplot(aes(x = log2(count), fill = method)) + 
+  geom_density()
+
+all_counts |> 
+  ggplot(aes(x = log2(count+1), fill = method)) + 
+  geom_density()
+
+# So log2 counts or counts+1 not a great way to visualize - see vst/rlog below
+# Can also summarize by group for total counts
+all_counts |> 
+   group_by(Sample, Group, method) |> 
+   summarise(total_reads = sum(count, na.rm = T) )  |> 
    ggplot(aes(x = Sample, y = total_reads, fill = Group)) + 
    geom_col() + 
    coord_flip() + 
@@ -52,6 +70,7 @@ vst <- varianceStabilizingTransformation(des, blind = T)
 rlog <- rlog(des, blind = T)
 
 DESeq2::plotPCA(vst, intgroup = 'Group')
+# can return the underlying data for the plot with returnData=T
 DESeq2::plotPCA(vst, intgroup = 'Group', returnData = T)
 plotPCA(rlog, intgroup = 'Group') 
 # Not perfect, but there is some grouping
@@ -61,9 +80,9 @@ plotPCA(rlog, intgroup = 'Group')
 # lots going on in this next line
 # assay(vst) - gets the variance stabilized count information
 # t() - transposes a matrix (i.e. makes the rows into columns)
-         #m <- matrix(c(1,2,3,4,5,6), nrow = 2)
-         #m
-         #t(m)
+         m <- matrix(c(1,2,3,4,5,6), nrow = 2)
+         m
+         t(m)
 # dist() - calculates distance between samples - way to look at groupings
 # as.matrix() - dist needs a matrix to operate on - so we convert out counts data to a matrix
 cor_samples <- as.matrix(dist(t(assay(vst)) )  )
@@ -71,12 +90,32 @@ cor_samples
 sample_df <- colData(vst) 
 
 sample_df
-anno_df <- sample_df %>% as.data.frame() %>% dplyr::select(Sample, Group) 
- # heatmap of similarities between samples - we'll discuss this in more detail next week
+anno_df <- sample_df |> as.data.frame() |> dplyr::select(Sample, Group) 
+#heatmap of similarities between samples - 
 hm_anno <-  HeatmapAnnotation(df  = sample_df$Group, col = list(df =c('A' = 'goldenrod', 'B' = 'steelblue') ) )
+# Heatmap comes from ComplexHeatmap
 Heatmap(cor_samples, top_annotation = hm_anno) 
 
+Heatmap(cor(cor_samples) ) 
 
+# A different data set - multiple groups and known replicates
+load('data/GSE102560_dds.Rda')
+# Need to know or remember what comes with an Rda object
+# In this case it is dds - which I'll rename to limit confusion
+swi_dds <- dds
+design(swi_dds)
+# Need to VST transform
+swi_vst <- varianceStabilizingTransformation(swi_dds, blind = T) 
+plotPCA(swi_vst, intgroup = 'condition')
 
+colData(swi_dds)
 
+pc_plot <- plotPCA(swi_vst, intgroup = 'condition', returnData = T)
+pc_plot
 
+pc_plot <- pc_plot  |> left_join(as.data.frame(colData(swi_dds)), by = c('name' = 'samples') ) 
+pc_plot |> 
+  ggplot(aes(x = PC1, y = PC2, color = group, shape = rep) ) + 
+  geom_point(size = 3)
+
+# Note the batch effect by replicate - class 6 will discuss how to deal with this. 
--- scripts/2022_scripts/visualize_deseq_results.R
+++ scripts/2022_scripts/visualize_deseq_results.R
@@ -1,24 +1,26 @@
 # Visualizing DESeq Results
 library(tidyverse)
-library(DESeq2)
+library(DESeq2) 
+BiocManager::install('apeglm') # need this to shrink log fold changes using method = 'apeglm'
 # Load our data from DESeq analysis
 load(file = 'data/DE_output.Rda')
 # Has two variables (des, res) 
-res <- results(des) 
 # Investigate the output
 resultsNames(des)
+design(des)
 summary(res) # this shows 28 genes go up and ~15 go down 
 #at an adjusted  p-value of 0.1
  
 # MA Plot 
-DESeq2::plotMA(res, ylim = c(-5,5))
-
+DESeq2::plotMA(res )
+DESeq2::plotMA(res, ylim = c(-10,10)) 
 #(notice the high lfc in the lower expression)
 # This is likely a poor estimate of the true fold chnage ( low expression with inflated lfc)
 # Need to specify which coefficient from our linear model to shrink
+shrunk_normal <- lfcShrink(des, coef = 2, type = 'normal') 
+# what is the coefficient? this comes from resultsNames(des)
 resultsNames(des) 
-shrunk <- lfcShrink(des, coef = 2, type = 'normal') 
-DESeq2::plotMA(shrunk, ylim = c(-5,5))
+DESeq2::plotMA(shrunk_normal, ylim = c(-5,5))
 
 shrunk <- lfcShrink(des, coef = 2, type = 'apeglm') 
 DESeq2::plotMA(shrunk, ylim = c(-5,5))
@@ -55,11 +57,13 @@ sdf %>%
    theme(panel.grid = element_blank()) + 
    geom_vline(aes(xintercept = 0), linetype = 'dashed', color ='grey30' )
 
+
 # label some points that are signficant
 top_10 <- sdf %>% arrange(padj) %>% head(10)
 top_10
-sdf %>%
-   mutate(padj = ifelse(is.na(padj), 1, padj) ) %>% # replace NAs with 1 for padj for this plot
+sdf <- sdf %>%
+   mutate(padj = ifelse(is.na(padj), 1, padj) )  # replace NAs with 1 for padj for this plot
+sdf |>  
    ggplot(aes(x = log2FoldChange, y = -log10(padj), color = padj < 0.1)) +
    geom_point() + 
    scale_color_manual(values = c('grey30', 'red2')) + 
@@ -68,20 +72,48 @@ sdf %>%
    geom_vline(aes(xintercept = 0), linetype = 'dashed', color ='grey30' ) + 
    ggrepel::geom_text_repel(aes(label = rowname), data= top_10, color = 'grey20') #new aesthetic, remember we can supply different dataframe here (top_10)
 
+
+# That's not the most helpful gene name
+# Can we fix that
+
+#Still have ensembl gene names
+#use annotables to fix this
+anno_df <- annotables::grch38 |> dplyr::select(ensgene, symbol) 
+sdf <- sdf |> left_join(anno_df, by = c('rowname' = 'ensgene'), multiple='first')
+top_10 <- sdf %>% arrange(padj) %>% head(10)
+sdf |>  
+  ggplot(aes(x = log2FoldChange, y = -log10(padj), color = padj < 0.1)) +
+  geom_point() + 
+  scale_color_manual(values = c('grey30', 'red2')) + 
+  theme_bw() + 
+  theme(panel.grid = element_blank()) + 
+  geom_vline(aes(xintercept = 0), linetype = 'dashed', color ='grey30' ) + 
+  ggrepel::geom_text_repel(aes(label = symbol), data= top_10, color = 'grey20') #new aesthetic, remember we can supply different dataframe here (top_10)
+
+
 # Now we can look at our results - we might specifically want to pick out our most significant genes and take a look
 sdf %>%
    filter(!is.na(padj) ) %>%
    arrange(padj) %>%
    head(30)
 
+#Need to change the names in the original rownames of the des
+rn <- rownames(des)
+rn_df <- data.frame(ensgene = rn) 
+# select just hte columns we want
+
+rn_df <- left_join(rn_df, anno_df, by = 'ensgene', multiple='first' )
+# multiple= 'first' , there are a few ens genes with more than one annotatation - this takes the first so that rownames match
+rownames(des) <- rn_df$symbol
+top_10
 # Plot some of these using plotCounts
-plotCounts(des, gene = 'DDX3Y', intgroup = 'Group', norm = T)
+plotCounts(des, gene = 'DDX3Y', intgroup = 'Group', norm = T) # DDX3Y
 plotCounts(des, gene = 'POP1', intgroup = 'Group', norm = T)
 plotCounts(des, gene = 'DDX3X', intgroup  = 'Group', norm = T) 
 
 # Threshold by an arbitrary logfoldchange cutoff
 res_filt <- results(des, lfcThreshold = abs(1) )
-# The above tests if the |LFC| is at least > 1, not if the LFC is different from 0
+  # The above tests if the |LFC| is at least > 1, not if the LFC is different from 0
 # This is NOT the same as filtering your genes for |LFC| > 0.6
 
 summary(res_filt)
@@ -91,7 +123,7 @@ DESeq2::plotMA(res_filt)
 norm_counts <- counts(des, norm = T) 
 
 # get a list of genes
-genes <- sdf %>% filter(padj < 0.1) %>% pull(rowname) # pull is a new verb I just learned! it selects one column and returns a vector
+genes <- sdf %>% filter(padj < 0.1) |>  filter(symbol != '') %>% pull(symbol) # pull is a new verb I just learned! it selects one column and returns a vector
 # keep just tne rows for those genes ( could use filters here as well, but b/c gene names are the rownames and not a column in the dataframe, the following is easier)
 dim(norm_counts)
 norm_counts <- norm_counts[rownames(norm_counts) %in% genes, ]
@@ -102,20 +134,26 @@ design <- colData(des)
 design
 column_ha <- HeatmapAnnotation(group = design$Group, 
                                col = list(group =  c("A" = 'Grey10', 'B' = 'Steelblue')) ) # I get the syntax for colors wrong here all the timek
+norm_counts
 Heatmap(norm_counts, top_annotation = column_ha) 
 # That heatmap basically jut shows which genes are highly exprssed (not scaled by row)
 vsd <- varianceStabilizingTransformation(des)
 vsd <- vsd[rownames(vsd) %in%  genes]
 Heatmap(as.matrix(assay(vsd)) , top_annotation = column_ha)
+
 # this is a little better, our scale is closer so easier to look at- things cluster better (b/c less affected by outliers)
 # But still mostly just showing which genes are high and which are low
-
+norm_counts
 # Scale heatmap by row - this is usually what you want  
 scaled_hm <- t(scale(t(norm_counts), center = T, scale = T) )  # lets z-score our data by row
+scaled_hm <- na.omit(scaled_hm)
 scaled_vsd <- t(scale(t(assay(vsd)), center = T, scale = T) )
+scaled_vsd <- na.omit(scaled_vsd)
+
 # Lots happening above, key function is scale
 column_ha <- HeatmapAnnotation(group = design$Group, 
                                col = list(group =  c("A" = 'Grey10', 'B' = 'Steelblue')) )
+
 Heatmap(scaled_hm, top_annotation = column_ha)  
 
 column_ha <- HeatmapAnnotation(group = design$Group, 
