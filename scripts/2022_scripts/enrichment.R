04cbc0022be1ae107d2035a4c77f5d2b14bfa333
Jesse Raab <jesse.r.raab@gmail.com>
2023-04-26 17:52:30 -0400

    pre class 7 updates

--- scripts/2022_scripts/enrichment.R
+++ scripts/2022_scripts/enrichment.R
@@ -3,17 +3,19 @@ library(tidyverse)
 library(msigdbr)  # install.packages('msigdbr')
 
 # Below packages can be installed with BiocManager::install(c('singscore', 'clusterProfiler', 'DOSE', 'org.Hs.eg.db') ) 
+# I tried to put these in a script to install - see enrichment_packages.R
 library(singscore) 
 library(clusterProfiler)
 library(enrichplot)
 library(biomaRt)
 library(GSVA)
 library(GSVAdata)
-library(UpSetR)
+library(UpSetR) 
 library(ggvenn)
 library(ggVennDiagram)
-
-# There is a current issue with RSqlite affecting loading of some packages
+library(fgsea)
+# There may be an issue with RSqlite affecting loading of some packages
+# If you get an issue with RSqlite - try the following
 #remotes::install_version("RSQLite", version = "2.2.5")
 # Either install the older version of rsqlite using above code or run 
 #options(connectionObserver=NULL) # prior to running the next line 
@@ -30,16 +32,19 @@ brg_res <- read_csv('/Users/jraab/GitHub/GNET749_RNAseq/data/results_brg1.csv')
 
 # Many of these tools require entrez geneIDs 
 # we can use bioMart to look these up and add to our results frame
-mart <- useMart(biomart = 'ensembl', dataset = 'hsapiens_gene_ensembl' )
-mart_res <- getBM(attributes = c('entrezgene_id', 'external_gene_name'), mart = mart) 
+#mart <- useMart(biomart = 'ensembl', dataset = 'hsapiens_gene_ensembl' )
+#mart_res <- getBM(attributes = c('entrezgene_id', 'external_gene_name'), mart = mart) 
 
 # an alternative is to use the bitr function from clusterprofiler. Included here for completeness
-##gene.df <- bitr(brg_res$rowname, fromType = "SYMBOL",
-               #toType = c("ENSEMBL", "SYMBOL", 'ENTREZID'),
-               #OrgDb = org.Hs.eg.db)
+gene.df <- bitr(brg_res$rowname, fromType = "SYMBOL",
+               toType = c("ENSEMBL", "SYMBOL", 'ENTREZID'),
+               OrgDb = org.Hs.eg.db)
+
+# Annotables could also be used if it has the annotations you want
+# Depending on which approach you use, you'll need to make sure column names line up in the code below
 
 # Add the entrez Ids
-brg_res <- brg_res %>% left_join(mart_res, by = c('rowname' = 'external_gene_name'))
+brg_res <- brg_res %>% left_join(gene.df, by = c('rowname' = 'SYMBOL'))
 head(brg_res)
 
 # Identify significant up and downregulated genes  - These are useful for overrepresentation analysis
@@ -51,16 +56,16 @@ brg1_res_ordered <- brg_res %>% arrange(desc(log2FoldChange))
 brg1_ol <- brg1_res_ordered$log2FoldChange
 brg1_ids <- brg1_res_ordered$log2FoldChange
 names(brg1_ol) <- brg1_res_ordered$rowname
-names(brg1_ids) <- brg1_res_ordered$entrezgene_id
+names(brg1_ids) <- brg1_res_ordered$ENTREZID
 
 # A few methods, (GSVA and SingScore) need full data (count/gene ranks)
-load('/Users/jraab/GitHub/GNET749_RNAseq/data/GSE102560_dds.Rda')# load dds
+load('data/GSE102560_dds.Rda')# load dds
 dds <- DESeq(dds) # running this here so we get proper size factors etc 
 dds_fix <- dds
 assay(dds_fix) <- limma::removeBatchEffect(assay(dds_fix), dds$rep)
 
 counts <- counts(dds_fix, norm = T)
-counts <- counts[rowMeans(counts) > 10, ]
+#counts <- counts[rowMeans(counts) > 10, ]
 dim(counts)
 ################################################################################
 # Following are various ways and resources for doing over representation analysis
@@ -68,57 +73,78 @@ dim(counts)
 # We're using genes that are up or downregulated by 1.5 fold
 
 # Over representation analysis using KEGG 
-kegg_up <- enrichKEGG(gene         = brg1_up$entrezgene_id, 
+kegg_up <-clusterProfiler::enrichKEGG(gene = brg1_up$ENTREZID, 
                       organism     = 'hsa',
                       pvalueCutoff = 0.05)
 
-kegg_down <- enrichKEGG(gene   = brg1_down$entrezgene_id, 
+kegg_down <- clusterProfiler::enrichKEGG(gene   = brg1_down$ENTREZID, 
                         organism = 'hsa', 
                         pvalueCutoff = 0.05)
 dotplot(kegg_up) 
 barplot(kegg_up)
 dotplot(kegg_down) 
 barplot(kegg_down)
+kegg_up
+kegg_up |> as.data.frame()
+kegg_up |> as.data.frame() |> 
+   ggplot(aes(x = -log10(qvalue), y = Description, size = Count)) + 
+   geom_point()
+
 
 # Over representation of GO terms
-brg_up_go <- enrichGO(gene    = brg1_up$entrezgene_id,
-                universe      = brg_res$entrezgene_id,
+brg_up_go <- clusterProfiler::enrichGO(gene    = brg1_up$ENTREZID,
+                universe      = brg_res$ENTREZID,
                 OrgDb         = org.Hs.eg.db,
                 ont           = c("MF"),
                 pAdjustMethod = "BH",
                 pvalueCutoff  = 0.01,
                 qvalueCutoff  = 0.05,
                 readable      = TRUE)
-dotplot(brg_up_go) 
 
-emapplot(pairwise_termsim(brg_up_go)) 
+brg_down_go <- clusterProfiler::enrichGO(gene    = brg1_down$ENTREZID,
+                universe      = brg_res$ENTREZID,
+                OrgDb         = org.Hs.eg.db,
+                ont           = c("MF"),
+                pAdjustMethod = "BH",
+                pvalueCutoff  = 0.01,
+                qvalueCutoff  = 0.05,
+                readable      = TRUE)
+
+dotplot(brg_up_go) 
+dotplot(brg_down_go)
+
+# termsim can be useful for seeing how similar categories are
+emapplot(pairwise_termsim(brg_down_go)) 
 emapplot(pairwise_termsim(kegg_down))
 # Can remove similar terms - does not work if using ont = 'ALL'
-dotplot(simplify(x = brg_up_go))
-
-brg_down_go <- enrichGO(gene          = brg1_down$entrezgene_id,
-                      universe      = brg_res$entrezgene_id,
-                      OrgDb         = org.Hs.eg.db,
-                      ont           = 'MF',
-                      pAdjustMethod = "BH",
-                      pvalueCutoff  = 0.01,
-                      qvalueCutoff  = 0.05,
-                      readable      = TRUE)
 dotplot(brg_down_go)
-dotplot(simplify(x = brg_down_go, by = 'p.adjust', cutoff = 0.7) )
+dotplot(simplify(x = brg_down_go))
+
+# Can look at gene expression within categories using heatplot
 heatplot(brg_down_go, foldChange = brg1_ol)  #not terribly interesting since we limited to up or downregulated genes
+
+brg_all_go <- enrichGO(gene = c(brg1_up$ENTREZID, brg1_down$ENTREZID), 
+                       universe      = brg_res$ENTREZID,
+                       OrgDb         = org.Hs.eg.db,
+                       ont           = 'MF',
+                       pAdjustMethod = "BH",
+                       pvalueCutoff  = 0.01,
+                       qvalueCutoff  = 0.05,
+                       readable      = TRUE)
+heatplot(brg_all_go, foldChange = brg1_ol)
 
 #  You can also do overrepresentation with any arbitrary gene set
 # Here we'll get the msigdb data from the msigdbr package   
 msigdbr_df <- msigdbr() # this retrieves all the data sets
 length(unique(msigdbr_df$gs_name) ) # ~32,000 gene sets
+msigdbr_df |> head()
 
 msigdbr_t2g = msigdbr_df %>% dplyr::select(gs_name, human_gene_symbol) %>% as.data.frame() # Make a df that contains the terms and genes
 msigdbr_t2g %>% head()
 
 # Generic enricher functions - run on the whole data set - not for class, too slow
-brg1_up_enr <- enricher(gene = brg1_up$rowname, TERM2GENE = msigdbr_t2g)
-brg1_down_enr <- enricher(gene = brg1_down$rowname, TERM2GENE = msigdbr_t2g)
+brg1_up_enr <- clusterProfiler::enricher(gene = brg1_up$rowname, TERM2GENE = msigdbr_t2g)
+brg1_down_enr <- clusterProfiler::enricher(gene = brg1_down$rowname, TERM2GENE = msigdbr_t2g)
 dotplot(brg1_up_enr)
 barplot(brg1_down_enr)
 cnetplot(brg1_up_enr)
@@ -140,6 +166,7 @@ emapplot(pairwise_termsim(e_hall) )
 #### Gene Set Enrichment Analysis - main benefit is that it does not require an arbitrary threshold ######
 brg1_hallmark <- GSEA(geneList = rev(sort(brg1_ol)), TERM2GENE = hallmark_t2g, by = 'fgsea')
 brg1_hallmark %>% as.data.frame() %>% head()
+
 
 brg1_hallmark %>%
    as.data.frame() %>% 
@@ -166,8 +193,9 @@ brg_res %>%
    filter(rowname %in% hallmark_subset) %>%
    ggplot(aes(x =log2FoldChange, y = -log10(padj), color = padj < 0.05 )) + 
    geom_point() + 
-   scale_color_manual(values = c('grey30', 'red2')) + 
-   theme_bw() 
+   scale_color_manual(values = c('grey70', 'red2')) + 
+   theme_bw()  + 
+   ggrepel::geom_label_repel(aes(label = rowname), color = 'steelblue')
 
 
 
@@ -182,10 +210,12 @@ brg_res %>%
 # We want our original data, which we can load from a saved DESeqDataSet
 # Remember counts is coming from the entire dds object we loaded up top
 rg <- rankGenes(counts) # this ranks the expression level in each of our samples
-
+rg
 # Now we pass this to simplescore along with a list of genes for our data set
 ss <- simpleScore(rg, upSet = unique(hallmark_subset) ) 
 ss
+
+table(rownames(rg) == 'CYR61')
 # Using this we can compare our scores across our samples
 # best to incorporate our conditions
 ss %>% as.data.frame() %>%
@@ -196,6 +226,7 @@ ss %>% as.data.frame() %>%
 #You can also give simplescore an upregulated and downregulated set
 # It's possible the hallmark set contains both, which blunts the score somewhat
 # HSIAO_LIVER_SPECIFIC_GENES
+
 liver_genes <- msigdbr_df[grepl('HSIAO_LIVER_SPECIFIC_GENES', msigdbr_df$gs_name), ] %>% dplyr::select(gs_name, gene_symbol)
 liver_genes
 ss_liver <- simpleScore(rg, upSet= unique(hallmark_subset), downSet = unique(liver_genes$gene_symbol) )
--- scripts/2022_scripts/enrichment_packages.R
+++ scripts/2022_scripts/enrichment_packages.R
@@ -1,7 +1,6 @@
 # Packages for Class 4
-BiocManager::install(c('DOSE', 'clusterProfiler', 'GSVA', 'singscore', 'enrichplot', 'biomaRt', 'msigdbr', 'limma', 'sva', 'GSVAdata', 'org.Hs.eg.db'))
-install.packages(c('UpSetR', 'vennDiagram', 'ggnewscale')) 
+BiocManager::install(c('DOSE', 'clusterProfiler', 'GSVA', 'singscore', 'enrichplot', 'biomaRt', 'msigdbr', 'limma', 'sva', 'GSVAdata', 'org.Hs.eg.db', 'fgsea'))
+install.packages(c('UpSetR', 'VennDiagram', 'ggnewscale')) 
 if (!require(devtools)) install.packages("devtools")
 devtools::install_github("yanlinlin82/ggvenn")
 devtools::install_github('gaospecial/ggVennDiagram')
-
