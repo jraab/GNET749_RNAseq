d49811f73d5a4e05f47579bb22b76ecbb41169b5
Jesse Raab <jesse.r.raab@gmail.com>
2024-04-22 13:08:56 -0400

    Update taylor.R

--- practice/taylor.R
+++ practice/taylor.R
@@ -2,9 +2,9 @@
 library(tidyverse)
 # download taylor facts from kaggle
 # save the zip file somewhere, and then unzip
-download.file(url = 'https://github.com/jraab/GNET749_RNAseq/tree/main/data/taylor_swift_spotify.csv', 
-              method = 'curl', 
-              destfile = '/Users/jraab/Desktop/taylor_swift_spotify.csv')
+download.file(url = download.file('https://raw.githubusercontent.com/jraab/GNET749_RNAseq/main/data/taylor_swift_spotify.csv', 
+                                  method = 'curl', 
+                                  destfile = '/Users/jraab/Desktop/taylor_swift_spotify.csv'))
 
 taylor <- read_csv('~/GitHub/GNET749_RNAseq/data/taylor_swift_spotify.csv')
 taylor
