1281074a592cff60f645fb36993eb6ba069fd366
Jesse Raab <jesse.r.raab@gmail.com>
2026-04-20 08:25:39 -0400

    pre class 2 update -  not final

--- scripts/class_packages.R
+++ scripts/class_packages.R
@@ -2,16 +2,16 @@
 # These are some packages we will use in this section
 # Jesse will update this if we need another packages
 if (!require("BiocManager", quietly = TRUE))
-BiocManager::install(version = "3.18")
-# R version 4.3.1 matches Bioconductor 3.18 (mismatching these can be an issue)
+BiocManager::install(version = "3.22")
+# R version 4.5.1 matches Bioconductor 3.22 (mismatching these can be an issue)
 
 
-cran_packages <- c('tidyverse', 'nycflights13', 'janitor', 'palmerpenguins', 'BiocManager', 'apeglm', 'ashr')
+cran_packages <- c('tidyverse', 'nycflights13', 'janitor', 'palmerpenguins', 'BiocManager', 'apeglm', 'ashr', 'taylor')
 biocon_packages <- c('DESeq2', 'sva', 'ComplexHeatmap', 
                      'tximport', 'biomaRt', 'limma', 'pasilla', 
                      'msigdbr', 'BiocFileCache')
 
 install.packages(cran_packages)
 BiocManager::install(biocon_packages )
-BiocManager::install('')
-
+#BiocManager::install('')
+ 
